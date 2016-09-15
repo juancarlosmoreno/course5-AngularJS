4ace773d291dcaad5af15daed36e963a4ebcea2d
Juan Carlos Moreno <juancarlos.jcm@gmail.com>
2016-09-15 20:18:23 +0200

    v1.0

--- Module1/app.js
+++ Module1/app.js
@@ -1,15 +1,46 @@
 (function () {
-	'use strict';
+'use strict';
 
-angular.module('myFirstApp', [])
+angular.module('LunchCheck', [])
 
-.controller('MyFirstController', function ($scope) {
+.controller('LunchCheckController', function ($scope) {
+  $scope.name = "";
+  $scope.totalValueOk = "";
+  $scope.totalValueFail1 = "";
+  $scope.totalValueFail2 = "";
 
-	$scope.sayHello = function () {
-		return "Hello Coursera!";
-	};
-	$scope.name = "Francisco López Alegría";
-	$scope.taller = "En la luna ...";
+  $scope.displayNumeric = function () {
+    var msg = calculatNumericForString($scope.name);
+    if(msg == 1){
+       $scope.totalValueOk = "Enjoy!";
+    }else if(msg==0){
+       $scope.totalValueFail1 = "Please enter data first";
+    }else{
+       $scope.totalValueFail2 = "Too much!";
+    }
+    // $scope.totalValue = calculatNumericForString($scope.name);
+  };
+
+
+  function calculatNumericForString(string) {
+    var list = [];
+    var msg = 0;
+    // var msg ="Please enter data first";
+    if(string==""){
+      return msg;
+    }
+    list = string.split(",");
+    if(list.length <= 3){
+      msg = 1;
+      // msg="Enjoy!";
+      }else{
+      msg = 2;
+      // msg = "Too much!";
+    }
+    return msg;
+  }
+
 });
+
 
 })();
